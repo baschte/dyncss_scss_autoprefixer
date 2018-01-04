a7fe8d78b4c37395a8ae8cad67cf6925bacfe26d
Sebastian Richter <sebastian.richter@pluswerk.ag>
2018-01-04 20:37:31 +0100

    [TASK] updating emconf and composer autoload // new icon

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,34 +1,30 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Dyncss - SCSS Autoprefixer',
-	'description' => 'Autoprefixer for Dyncss Scss',
-	'shy' => 0,
-	'version' => '0.1.0',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Sebastian Richter',
-	'author_email' => 'info@baschte.de',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3'  => '6.2.0-9.0.0',
-			'dyncss'  => '0.8.0-2.0.9',
-			'dyncss_scss'  => '1.0.0-2.0.99',
-		),
-	),
-	'_md5_values_when_last_written' => 'a:0:{}',
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Dyncss - SCSS Autoprefixer',
+    'description' => 'Autoprefixer for Dyncss Scss',
+    'category' => 'frontend',
+    'constraints' => [
+        'depends' => [
+            'typo3'  => '6.2.0-9.0.0',
+            'dyncss'  => '0.8.0-2.0.9',
+            'dyncss_scss'  => '1.0.0-2.0.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Baschte\\DyncssScssAutoprefixer\\' => 'Classes'
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+		'author' => 'Sebastian Richter',
+		'author_email' => 'info@baschte.de',
+    'version' => '0.1.1',
+];
 
 ?>
