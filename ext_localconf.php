d0dea96b5d2ef77efdf595e79968e99970950789
Sebastian Richter <sebastian.richter@pluswerk.ag>
2018-01-05 09:24:47 +0100

    [BUGFIX] register SignalSlotDispatcher for dyncss

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,15 @@
 <?php
 
-if(class_exists('\KayStrobach\Dyncss\Configuration\BeRegistry')) {
-	\KayStrobach\Dyncss\Configuration\BeRegistry::get()->registerFileHandler('scss', 'Baschte\DyncssScssAutoprefixer\Parser\ScssParser');
+if (TYPO3_MODE === 'BE') {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['SCSS Autoprefixer'][] =
+        \Baschte\DyncssScssAutoprefixer\Reports\StatusReport::class;
 }
 
-if (TYPO3_MODE === 'BE') {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['SCSS Autoprefixer'][] =
-		\Baschte\DyncssScssAutoprefixer\Reports\StatusReport::class;
-}
+/** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
+$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+$signalSlotDispatcher->connect(
+    \KayStrobach\Dyncss\Parser\AbstractParser::class,  // Signal class name
+    'afterFileParsed',                                  // Signal name
+    \Baschte\DyncssScssAutoprefixer\Slots\FileParsedSlot::class,        // Slot class name
+    'afterFileParsed'                               // Slot name
+);
